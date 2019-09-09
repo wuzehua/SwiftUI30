33cb0bcff87e7bc66b30b1e3b20673839b8ef5dd
RainZhong <3170104171@zju.edu.cn>
2019-09-09 17:17:37 +0800

    Initial Commit

---
+++ SwiftUI30/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI30
+//
+//  Created by RainZhong on 2019/9/9.
+//  Copyright © 2019 RainZhong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
