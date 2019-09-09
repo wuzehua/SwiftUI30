10b14bd735ce65a5fc94ea2e061bc7d5ab494d0d
RainZhong <3170104171@zju.edu.cn>
2019-09-09 23:32:24 +0800

    add Typeface Comparison

---
+++ SwiftUI30/SubPage/TypefaceComparison.swift
@@ -0,0 +1,21 @@
+//
+//  Typeface Comparison.swift
+//  SwiftUI30
+//
+//  Created by RainZhong on 2019/9/9.
+//  Copyright © 2019 RainZhong. All rights reserved.
+//
+
+import SwiftUI
+
+struct TypefaceComparison: View {
+    var body: some View {
+        Text("hhh")
+    }
+}
+
+struct TypefaceComparison_Previews: PreviewProvider {
+    static var previews: some View {
+        TypefaceComparison()
+    }
+}
